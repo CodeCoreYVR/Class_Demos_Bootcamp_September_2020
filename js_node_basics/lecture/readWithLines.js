92b50bdf4ca3a0c409281529af66d5a8acc163f8
Naveed Raza <naveedraza@gmail.com>
2020-10-15 14:42:43 -0700

    readWithLinesModified added

--- js_node_basics/lecture/readWithLines.js
+++ js_node_basics/lecture/readWithLines.js
@@ -1,8 +1,16 @@
 const fs=require('fs');
 
 const fileName=process.argv[2];
+// const name=process.argv[3];
+// console.log(name)
+// const fileName="poem.txt"
 
 fs.readFile(fileName,(err,data)=>{
+    if(err){
+        console.log('File Not found')
+        process.exit(-1);
+    }
+    console.log(data);
     const fileContents=data.toString();
     console.log(fileContents)
     const lineArray=fileContents.split('\n');
