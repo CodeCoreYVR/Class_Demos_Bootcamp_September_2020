1d0022b5df960e0e6711cba227f58df3f7ddb981
Naveed Raza <naveedraza@gmail.com>
2020-10-22 14:06:57 -0700

    Class Exercises

--- js_knex_demo/delete.js
+++ js_knex_demo/delete.js
@@ -1,9 +1,23 @@
-const knex=require('./db/client');
+const knex = require('./db/client');
+// Delete One Post
+if (false) {
+    knex('posts')
+        .where("id", 4)
+        .del()
+        .then(result => {
+            console.log(result);
+            knex.destroy();
+        })
+}
+// Delete all posts but one
+const deleteQuery = knex("posts")
+    .where("id", "!=", "1")
+    .del();
 
-knex('posts')
-.where("id",4)
-.del()
-.then(result=>{
-    console.log(result);
+console.log(deleteQuery.toString());
+
+deleteQuery.then(result => {
+    console.log("deleteQuery: ", deleteQuery);
+    console.log("result: ", result);
     knex.destroy();
-})
+});
--- js_knex_demo/select.js
+++ js_knex_demo/select.js
@@ -1,6 +1,6 @@
-const { truncate } = require('./db/client');
 const knex = require('./db/client'); // used for connection
-if (false) {
+
+if (true) {
     knex
         .select('*') // "*" is a wild card to say select all
         .from('posts') // passing name of the table
@@ -11,70 +11,100 @@ if (false) {
             }
         );
 }
-if(false){
+if (false) {
     knex
-    .select('title','content')
-    .from("posts")
-    .then(result=>{
-        console.log(result);
-        knex.destroy();
-    })
+        .select('title', 'content')
+        .from("posts")
+        .then(result => {
+            console.log(result);
+            knex.destroy();
+        })
 
 }
 // Select with Filters
 
 
 // Where clause with %% - wildcards and ilike
-if(true){
-    const queryString=knex
-    .select('title','content')
-    .from("posts")
-    .where('title','ilike','%rs%'); // % is again a wild card used as filter and it means title having 'rs' in middle will be fetched
-    
+if (false) {
+    const queryString = knex
+        .select('title', 'content')
+        .from("posts")
+        .where('title', 'ilike', '%rs%'); // % is again a wild card used as filter and it means title having 'rs' in middle will be fetched
+
     console.log(queryString.toString());
-    queryString.then(result=>{
+    queryString.then(result => {
         console.log(result);
         knex.destroy();
     })
 }
 // limit and offset - We use this for pagination
-if(false){
+if (false) {
     knex
-    .select('title','content')
-    .from("posts")
-    .limit(5)
-    .offset(2)
-    .then(result=>{
-        console.log(result);
-        knex.destroy();
-    })
+        .select('title', 'content')
+        .from("posts")
+        .limit(5)
+        .offset(2)
+        .then(result => {
+            console.log(result);
+            knex.destroy();
+        })
 }
 
 //Raw SQL in select
-if(false){
+if (false) {
     knex
-    .select("*")
-    .from("posts")
-    .whereRaw(`title ilike ? and content ilike ?`,['T%','%.'])
-    // .whereRaw(`title ilike ?`,["T%"])
-    // .whereRaw(`title ilike 'T%' and content ilike '%.'`)
+        .select("*")
+        .from("posts")
+        .whereRaw(`title ilike ? and content ilike ?`, ['T%', '%.'])
+        // .whereRaw(`title ilike ?`,["T%"])
+        // .whereRaw(`title ilike 'T%' and content ilike '%.'`)
 
-    .then(result=>{
-        console.log(result);
-        knex.destroy();
-    })
+        .then(result => {
+            console.log(result);
+            knex.destroy();
+        })
 }
 
 // Aggregate Functions
-if(false){
+if (false) {
     knex
-    .count({postcount: "*"})
-    .avg({ averageViews: "views"})
-    .max("views as highestViewCount")
-    .from('posts')
-    .then(result=>{
-        console.log(result);
-        knex.destroy();
-    })
+        .count({
+            postcount: "*"
+        })
+        .avg({
+            averageViews: "views"
+        })
+        .max("views as highestViewCount")
+        .from('posts')
+        .then(result => {
+            console.log(result);
+            knex.destroy();
+        })
 
+}
+//Class Exercise
+
+if (false) {
+    knex
+        .select('*')
+        .from('posts')
+        .whereRaw(`"createdAt" > now()-interval '2 days'`)
+        .orderBy('createdAt', 'desc') // other option is asc
+        .then(data => {
+            console.log(data);
+            knex.destroy();
+        })
+}
+
+// Exercise Title ends with letter 't'
+if (false) {
+    knex
+        .select('*')
+        .from('posts')
+        .where("title", 'ilike', '%t')
+
+        .then(data => {
+            console.log(data)
+            knex.destroy();
+        })
 }
