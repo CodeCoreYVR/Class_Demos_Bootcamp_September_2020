3c6236c192d8c8e425cfa943be3f99f65ef00a05
Naveed Raza <naveedraza@gmail.com>
2020-12-08 11:26:24 -0800

    Lab 4 - Solution

--- js_event_propogation_jquery/labs/jquery_lab/main.js
+++ js_event_propogation_jquery/labs/jquery_lab/main.js
@@ -1,87 +1,112 @@
 $(document).ready(() => {
-// LAB 1
-// $('.shape').on('mouseenter',(event)=>{
-//     $(event.currentTarget).toggleClass('highlight')
-// })
+// // LAB 1
+// // $('.shape').on('mouseenter',(event)=>{
+// //     $(event.currentTarget).toggleClass('highlight')
+// // })
 
-// Solution B:
-$('.shape').on('mouseenter', function() {
-	$(this).toggleClass('highlight');
-});
+// // Solution B:
+// $('.shape').on('mouseenter', function() {
+// 	$(this).toggleClass('highlight');
+// });
 
-// "Regular"
+// // "Regular"
 
-$('.shape.small').on('click', function() {
-	$(this).hide();
-});
+// $('.shape.small').on('click', function() {
+// 	$(this).hide();
+// });
 
-$('.shape.medium').on('click', e => {
-	$(e.currentTarget)
-		.removeClass('medium')
-		.addClass('small');
-});
+// $('.shape.medium').on('click', e => {
+// 	$(e.currentTarget)
+// 		.removeClass('medium')
+// 		.addClass('small');
+// });
 
-$('.shape.large').on('click', e => {
-	$(e.currentTarget)
-		.removeClass('large')
-		.addClass('medium');
-});
-
-
-
-// LAB 2: 
-
-// Prepend a row to the table with the columns 0 and -
-$('tbody').prepend('<tr><td>0</td><td>-</td></tr>');
-
-// When the form's submit button is clicked, append the text input's current value to the form message.
-$('#form-1').submit(function(event) {
-  $('#form-message').append(
-    $(this)
-      .find('input[type=text]')
-      .val()
-  );
-});
-
-// When Button 1 is clicked, toggle the green container.
-$('#button-1').on('click', e => {
-  $('#green-container').toggle();
-});
-
-// When Button 2 is clicked, fade the Button Message out
-$('#button-2').on('click', e => {
-  $('#button-message').fadeOut();
-});
-
-// When Button 3 is clicked, fade the Button Message back in.
-$('#button-3').on('click', e => {
-  $('#button-message').fadeIn();
-});
-
-// When Button 4 is clicked, slide the green container up.
-$('#button-4').on('click', e => {
-  $('#green-container').slideUp();
-});
+// $('.shape.large').on('click', e => {
+// 	$(e.currentTarget)
+// 		.removeClass('large')
+// 		.addClass('medium');
+// });
 
 
-// Lab 3 : 
-// When the g key is pressed, toggle all gray shapes.
 
-$('body').on('keypress', event => { //'keydown'
-	const { key } = event;
-	if (key === 'g') {
-		$('.shape.grey').toggle();
-	} else if (key === ' ') {
-		// When the spacebar key is pressed, append a small blue circle to the green container.
-		$('#green-container').append('<div class="small blue circle shape"></div>');
+// // LAB 2: 
+
+// // Prepend a row to the table with the columns 0 and -
+// $('tbody').prepend('<tr><td>0</td><td>-</td></tr>');
+
+// // When the form's submit button is clicked, append the text input's current value to the form message.
+// $('#form-1').submit(function(event) {
+//   $('#form-message').append(
+//     $(this)
+//       .find('input[type=text]')
+//       .val()
+//   );
+// });
+
+// // When Button 1 is clicked, toggle the green container.
+// $('#button-1').on('click', e => {
+//   $('#green-container').toggle();
+// });
+
+// // When Button 2 is clicked, fade the Button Message out
+// $('#button-2').on('click', e => {
+//   $('#button-message').fadeOut();
+// });
+
+// // When Button 3 is clicked, fade the Button Message back in.
+// $('#button-3').on('click', e => {
+//   $('#button-message').fadeIn();
+// });
+
+// // When Button 4 is clicked, slide the green container up.
+// $('#button-4').on('click', e => {
+//   $('#green-container').slideUp();
+// });
+
+
+// // Lab 3 : 
+// // When the g key is pressed, toggle all gray shapes.
+
+// $('body').on('keypress', event => { //'keydown'
+// 	const { key } = event;
+// 	if (key === 'g') {
+// 		$('.shape.grey').toggle();
+// 	} else if (key === ' ') {
+// 		// When the spacebar key is pressed, append a small blue circle to the green container.
+// 		$('#green-container').append('<div class="small blue circle shape"></div>');
+// 	}
+// });
+
+// // Make the Form Message show the number of characters remaining
+// // (14 characters maximum) as you type in the text input. (e.g. "3 characters remaining").
+
+// $('#form-1 > input[type=text]').on('input', function(event) {
+// 	let remaining = 14 - $(this).val().length;
+// 	$('#form-message').text(`${remaining} characters remaining`);
+// });
+
+// LAB 4:
+
+$('#form-1').submit(function(e) {
+	const color = $('#form-1 > input[type=text]').val();
+
+	// If they enter an invalid color show them an alert message telling them,
+	// "Entered color is not a valid option!"
+	const validColors = ['red', 'blue', 'black', 'grey'];
+
+	if (!validColors.includes(color)) {
+		alert(`Entered colour, "${color}", is not a valid option!`);
+	} else {
+		// All shapes matching the given color should be removed.
+		$(`.shape.${color}`).remove();
 	}
-});
 
-// Make the Form Message show the number of characters remaining
-// (14 characters maximum) as you type in the text input. (e.g. "3 characters remaining").
-
-$('#form-1 > input[type=text]').on('input', function(event) {
-	let remaining = 14 - $(this).val().length;
-	$('#form-message').text(`${remaining} characters remaining`);
+	// Clear the input field.
+	this.reset(); /* 
+		This really clears the entire form, but since the form only has one input,
+		it is equivalent.
+	*/
+	// or
+	// $('#form-1 > input[type=text]').val('');
 });
 });
