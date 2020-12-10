62587afa1e97a640e3c4a601d9038c4ce5ebba79
Brandon Y Lam <me@brandonlam.ca>
2020-12-10 14:23:10 -0800

    add create question feature and mock sign in

--- spa/scripts/main.js
+++ spa/scripts/main.js
@@ -8,17 +8,65 @@ const Question = {
         console.log(res);
         return res.json();
       })
+  },
+
+  create(params) {
+    return fetch(`${BASE_URL}/questions`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
+    }).then((res) => res.json());
   }
 }
 
-Question.index()
-  .then(questions => {
-    const questionsContainer = document.querySelector('ul.question-list');
-    questionsContainer.innerHTML = questions.map(q => {
-      return `
-        <li>
-          ${q.id} - ${q.title}
-        </li>
-      `
-    }).join('');
-  })
+const Session = {
+  create(params) {
+    return fetch(`${BASE_URL}/sessions`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
+    })
+  }
+}
+
+Session.create({
+  email: 'js@winterfell.gov',
+  password: 'supersecret'
+}).then(console.log);
+
+function loadQuestions() {
+  Question.index()
+    .then(questions => {
+      const questionsContainer = document.querySelector('ul.question-list');
+      questionsContainer.innerHTML = questions.map(q => {
+        return `
+          <li>
+            ${q.id} - ${q.title}
+          </li>
+        `
+      }).join('');
+    })
+}
+
+loadQuestions();
+
+const newQuestionForm = document.querySelector('#new-question-form');
+newQuestionForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const form = event.currentTarget
+  const formData = new FormData(form);
+  const newQuestionParams = {
+    title: formData.get('title'),
+    body: formData.get('body')
+  }
+  Question.create(newQuestionParams)
+    .then(data => {
+      loadQuestions();
+    })
+})
