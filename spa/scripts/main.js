88c5ff143195a8f5b5ab0666942cd7b65974474a
Brandon Y Lam <me@brandonlam.ca>
2020-12-10 13:40:30 -0800

    questions index

--- spa/scripts/main.js
+++ spa/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+const BASE_URL = `http://localhost:3000/api/v1`;
+
+const Question = {
+  index() {
+    return fetch(`${BASE_URL}/questions`)
+      .then(res => {
+        // res object has a method .json() that will parse the body of response and return it as json
+        console.log(res);
+        return res.json();
+      })
+  }
+}
+
+Question.index()
+  .then(questions => {
+    const questionsContainer = document.querySelector('ul.question-list');
+    questionsContainer.innerHTML = questions.map(q => {
+      return `
+        <li>
+          ${q.id} - ${q.title}
+        </li>
+      `
+    }).join('');
+  })
