d869ee5b7fb3d320a2d6182d0ac4c8c1b21cbf65
Naveed Raza <naveedraza@gmail.com>
2020-12-14 15:59:16 -0800

    SPA 2 Update and Delete

--- spa/scripts/main.js
+++ spa/scripts/main.js
@@ -5,7 +5,7 @@ const Question = {
     return fetch(`${BASE_URL}/questions`)
       .then(res => {
         // res object has a method .json() that will parse the body of response and return it as json
-        console.log(res);
+        // console.log(res);
         return res.json();
       })
   },
@@ -24,7 +24,26 @@ const Question = {
   show(id) {
     return fetch(`${BASE_URL}/questions/${id}`)
       .then(res => res.json());
-  }
+  },
+  update(id, params) {
+    return fetch(`${BASE_URL}/questions/${id}`, {
+      method: 'PATCH',
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(params)
+    }).then(res => res.json());
+  },
+  destroy(id){
+    return fetch(`${BASE_URL}/questions/${id}`,{
+      method:"DELETE",
+      credentials:'include',
+    }).then(res=>res.json())
+
+    }
+  
+
 }
 
 const Session = {
@@ -119,7 +138,7 @@ navbar.addEventListener('click', (event) => {
     navigateTo(page);
   }
 });
-
+// <div data-* ='value'                *=naveed *=name *=id *=target
 // question show
 const questionsContainer = document.querySelector('ul.question-list');
 questionsContainer.addEventListener('click', event => {
@@ -139,9 +158,81 @@ function renderQuestionShow(id) {
         <h2>${question.title}</h2>
         <p>${question.body}</p>
         <small>Authored by: ${question.author.first_name} ${question.author.last_name}</small>
-        <small>Liked by: ${question.like_count}</small>
+        <small>Liked by: ${question.like_count}</small><br>
+        <a data-target="question-edit" data-id="${question.id}" href="">Edit</a> <br>
+        <a data-target="delete-question" data-id="${question.id}" href="">Delete</a>
       `
+      // Step 1: Edit Question Create link 👆🏻
       showPage.innerHTML = questionHTML;
       navigateTo('question-show');
     })
 }
+
+/* Steps:
+ 1. Edit Link - On show page
+ Select a link  with query selector and then add a Event listner 'click'
+ on Click we need a id of a question
+ 2. On click a form filled with data of that particular question qhould be displayed
+ 3. Now user should be able to make changes
+ 4. User will press update button which will submit that form 
+ 5. Show page displaying new data should be displayed on a screen
+*/
+
+// Select a link  with query selector and then add a Event listner 'click'
+//  on Click we need a id of a question 
+
+
+document.querySelector('#question-show').addEventListener('click', (event) => {
+  event.preventDefault();
+  const questionID = event.target.dataset.id
+  const actionNeededToBePerformed = event.target.dataset.target
+  if (questionID) {
+    // console.log(editQuestionID);
+    if (actionNeededToBePerformed === 'delete-question') {
+      console.log(`Delete: ${questionID}`)
+      Question.destroy(questionID).then(question=>{
+        navigateTo('question-index');
+      })
+    } else {
+      console.log(`Edit: ${questionID}`)
+      populateForm(questionID); // and then loading an edit form with data with populateForm() 
+      navigateTo('question-edit'); // Displaying edit form with filled data
+    }
+  }
+})
+
+function populateForm(id) {
+  Question.show(id).then(questionData => {
+      // console.log(questionData)
+      document.querySelector('#edit-question-form [name=title]').value = questionData.title;
+      document.querySelector('#edit-question-form [name=body]').value = questionData.body;
+      document.querySelector('#edit-question-form [name=id]').value = questionData.id;
+    }
+
+  )
+}
+
+const editQuestionForm = document.querySelector('#edit-question-form'); // Selecting a form from this querySelector using ID
+editQuestionForm.addEventListener('submit', (event) => { // Attaching a submit event with this form
+  event.preventDefault();
+  const editFormData = new FormData(event.currentTarget); // grabbing data from the Form using FormData
+  const updatedQuestionParams = { // Creating object of edited data
+    title: editFormData.get('title'),
+    body: editFormData.get('body')
+  }
+  // console.log(updatedQuestionParams);
+  Question.update(editFormData.get('id'), updatedQuestionParams) // using update method of question to submit form new data
+    .then(question => {
+      editQuestionForm.reset();
+      renderQuestionShow(question.id);
+    })
+})
+
+// Steps for Deleting a Question:
+/*
+1. Creating a link of delete
+2. We need a id of the particular question that we have to delete.
+3. it can be redirected to the index page once delete is performed
+
+
+*/
